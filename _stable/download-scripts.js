71b22e95bd543d42252f21b193209bebf518c948
Jacques Murray <jacquesmmurray@gmail.com>
2022-03-04 18:28:59 +0200

    Create download-script.js

---
+++ _stable/download-scripts.js
@@ -0,0 +1,108 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/ap-hacknet-node.js",
+    "ap-hacknet-node.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/aps-lite.js",
+    "aps-lite.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/auto-deploy.js",
+    "auto-deploy.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/auto-purchase-server.js",
+    "auto-purchase-server.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/auto-starter.js",
+    "auto-starter.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/book-keeper.js",
+    "book-keeper.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/captain.js",
+    "captain.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/diamond-hands.js",
+    "diamond-hands.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/find-server.js",
+    "find-server.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/find-targets.js",
+    "find-targets.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/gimme-money.js",
+    "gimme-money.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/gtfo.js",
+    "gtfo.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/launch-fleets.js",
+    "launch-fleets.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/pirate.js",
+    "pirate.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/port-utils.js",
+    "port-utils.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/probe.js",
+    "probe.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/queue-service.js",
+    "queue-service.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/strategist.js",
+    "strategist.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/utils.js",
+    "utils.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/warmonger.js",
+    "warmonger.js",
+    "home"
+  );
+  await ns.wget(
+    "https://raw.githubusercontent.com/Jacques-Murray/bitburner-automation/main/_stable/watchtower.js",
+    "watchtower.js",
+    "home"
+  );
+}
