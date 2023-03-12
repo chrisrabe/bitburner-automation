19502b952ebe70e391b282aa73f15cddaf157c06
Brent Kerlin <bkerlin@users.noreply.github.com>
2023-03-11 22:06:15 -0500

    Update git-download.js
    Updated to current list of scripts.

--- community/Kadwen01/git-download.js
+++ community/Kadwen01/git-download.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 
-  	let list =[
+  let list =[
         'ap-hacknet-node.js',
         'aps-lite.js',
         'auto-deploy.js',
@@ -9,11 +9,22 @@ export async function main(ns) {
         'auto-starter.js',
         'book-keeper.js',
         'captain.js',
+        'corp-division-manager.js',
+        'corp-product-manager.js',
+        'corp-recruiter.js',
+        'corp-researcher.js',
+        'dev-tools-v2.js',
+        'dev-tools.js',
         'diamond-hands.js',
         'find-server.js',
         'find-targets.js',
+        'get-yt-comments.js',
         'gimme-money.js',
+        'gimme-more-money.js',
+        'grow-pirate.js',
         'gtfo.js',
+        'hack-pirate.js',
+        'kill-network-scripts.js',
         'launch-fleets.js',
         'pirate.js',
         'port-utils.js',
@@ -22,8 +33,10 @@ export async function main(ns) {
         'strategist.js',
         'utils.js',
         'warmonger.js',
-        'watchtower.js'        
-    ];
+        'watchtower.js',
+        'weaken-pirate.js',
+        'wgetCS.js'
+  ];
 
   for (const script of list) {
     ns.tprintf ('Pulling ' + script + ' off gitHub');
